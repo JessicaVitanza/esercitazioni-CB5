953c3dd0626fa2583e7e3d74f369114dd293815a
JessicaVitanza <jessicavitanzauidesign@gmail.com>
2022-12-20 15:30:54 +0100

    1° react commit

--- 20-12-22/src/App.js
+++ 20-12-22/src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import { Button } from "./components/Button";
 import './App.css';
 
@@ -6,21 +5,11 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+        Hello World!
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
         <Button />
       </header>
-      
     </div>
   );
 }
--- 20-12-22/src/components/Button.jsx
+++ 20-12-22/src/components/Button.jsx
@@ -1,15 +1,19 @@
 export function Button() {
     const log = () => {
-      console.log("Mi hai cliccato!");
+      console.log("Hello World!");
+      window.alert('Ciao Mondo!');
+        const AppHeader = document.querySelector(".App-header");
+        const textContainer = document.createElement("div");
+        const textParagraf = document.createElement("p");
+
+        textContainer.className = "textContainer";
+        textParagraf.textContent = "Ciao Mondo!";
+
+        textContainer.append(textParagraf);
+        AppHeader.append(textContainer);
     };
   
     return (
-      <button
-        aria-label="Click me for show a console log"
-        className="btn"
-        onClick={log}
-      >
-        Click me!
-      </button>
+      <button aria-label="Click me for show a console log" className="btn" onClick={log}>Click me!</button>
     );
   }
