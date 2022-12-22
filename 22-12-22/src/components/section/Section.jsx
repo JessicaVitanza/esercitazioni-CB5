c3d7fdf33566668ff47189d16070b33526bb9062
JessicaVitanza <jessicavitanzauidesign@gmail.com>
2022-12-22 18:04:24 +0100

    3° react commit

--- 22-12-22/src/components/section/Section.jsx
+++ 22-12-22/src/components/section/Section.jsx
@@ -1,25 +1,28 @@
 import './section.scss';
 import { Button } from '../../atoms/button/Button';
+import { Text } from '../../atoms/text/Text';
 
 export const Section = () => {
 
     return (   
         <section className='Section'>
 
-           <p>Make your choice:</p>   
+            <Text as='h1'
+                  variant='title'
+                  >Make your choice:</Text>   
            
            <div className='ButtonDiv'>
 
               <Button aria-label="Do not touch!" 
-              variant='primary'
-              className='no-border'
-              onClick={() => console.log('Ti avevo detto di non premere!')}
-              >Don't click!</Button>
+                      variant='primary'
+                      className='no-border'
+                      onClick={() => console.log('Ti avevo detto di non premere!')}
+                      >Don't click!</Button>
           
               <Button variant='secondary'
-              className='no-border'
-              onClick={() => console.log('Bravo/a! Hai cliccato sul bottone')}
-              >Click here!</Button> 
+                      className='no-border'
+                      onClick={() => console.log('Bravo/a! Hai cliccato sul bottone')}
+                      >Click here!</Button> 
             </div>
 
         </section>
